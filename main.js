0af66f0c64b9e6ae0d95b81edaa696f0eb97c669
Msa360 <59627629+Msa360@users.noreply.github.com>
2022-05-19 19:26:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,32 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 
-let M = 0.05; // mutation rate
+let M = 0.1; // mutation rate
 let N = 100; // number of cars
+
+// assign N and M or if not store N and M 
+if (sessionStorage.getItem("varM") && sessionStorage.getItem("varN")) {
+    M = sessionStorage.getItem("varM");
+    N = sessionStorage.getItem("varN");
+} else {
+    sessionStorage.setItem("varM", M);
+    sessionStorage.setItem("varN", N)
+}
+
+// changing mutation rate and number of cars
+function changeParams() {
+    if (document.getElementById("varM").value != "" && !isNaN(document.getElementById("varM").value) && document.getElementById("varM").value != 0) {
+        sessionStorage.setItem("varM", document.getElementById("varM").value);
+    }
+    if (document.getElementById("varN").value != "" && !isNaN(document.getElementById("varN").value)) {
+        sessionStorage.setItem("varN", document.getElementById("varN").value);
+    }
+}
+
+// display update N and M on the html form
+document.getElementById("varN").value = N;
+document.getElementById("varM").value = M;
+
 const cars = generateCars(N);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
@@ -82,7 +106,7 @@ function animate(time) {
 
     carCtx.restore();
 
-    networkCtx.lineDashOffset = (-time/50)*Math.abs(bestCar.speed);
+    networkCtx.lineDashOffset = (-time/50)*(Math.abs(bestCar.speed) != 0 ? 1 : 0);
 
     Visualizer.drawNetwork(networkCtx, bestCar.brain);
 
